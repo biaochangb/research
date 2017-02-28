7fc6caa095911efcac5a64d4773417b758739a44
Biao CHANG <985966377@qq.com>
2017-02-28 21:15:48 +0800

    modify map_bfsa by mutliprocessing

--- SourceDetection/src/map_bfsa.py
+++ SourceDetection/src/map_bfsa.py
@@ -8,8 +8,22 @@ created at 2017/2/27.
 
 import networkx as nx
 import numpy as np
-
+from decimal import *
 import method
+import itertools
+import multiprocessing
+
+import copy_reg
+import types
+from functools import partial
+
+def _pickle_method(m):
+    if m.im_self is None:
+        return getattr, (m.im_class, m.im_func.func_name)
+    else:
+        return getattr, (m.im_self, m.im_func.func_name)
+
+copy_reg.pickle(types.MethodType, _pickle_method)
 
 
 class BFSA(method.Method):
@@ -26,6 +40,7 @@ class BFSA(method.Method):
         self.prior_detector = prior_detector
         self.bfs_trees = {}
         self.likelihoods = {}
+        self.permutation_likelihood = {}
         self.depths = {}
         self.descendants = {}
 
@@ -40,24 +55,38 @@ class BFSA(method.Method):
         self.prior_detector.set_data(self.data)
         self.prior_detector.detect()
         self.prior = nx.get_node_attributes(self.subgraph, 'centrality')
-        print self.prior
 
         self.reset_centrality()
         nodes = self.subgraph.nodes()
+        self.weights = self.data.weights
+        self.node2index = self.data.node2index
+        n = len(nodes)
         for v in nodes:
             self.bfs_trees[v] = nx.bfs_tree(self.subgraph, v)
-            print v, self.bfs_trees[v].edges()
-            self.likelihoods[v] = 0
+            self.likelihoods[v] = multiprocessing.Value('d', 0.0)
+            self.permutation_likelihood[v] = list()
+            #self.values[v] = multiprocessing.Value('d', 0.0)
             self.descendants[v] = {}
             self.depths[v] = {}
             self.visited.clear()
             self.find_descendants_bfs(v, v)
-            #self.depths[v] = nx.single_source_dijkstra_path_length(self.bfs_trees[v], v, weight=None)
-        print self.depths
-        self.get_likelihood_by_BFSA(nodes, 0, len(nodes) - 1)
-        posterior = {v: self.prior[v] * self.likelihoods[v] for v in nodes}
-        nx.set_node_attributes(self.subgraph, 'centrality', posterior)
 
+        multiprocessing.freeze_support()
+        processes = list()
+        for i in np.arange(0, n):
+            nodes[0], nodes[i] = nodes[i], nodes[0]
+            p = multiprocessing.Process(target=self.get_likelihood_by_BFSA, args=(nodes, 1, n - 1))
+            p.start()
+            processes.append(p)
+        for p in processes:
+            p.join()
+
+        #self.get_likelihood_by_BFSA2(nodes)
+        #self.get_likelihood_by_BFSA(nodes, 0, len(nodes) - 1)
+
+        posterior = {v: self.prior[v] * Decimal(self.likelihoods[v].value) for v in nodes}
+
+        nx.set_node_attributes(self.subgraph, 'centrality', posterior)
         return self.sort_nodes_by_centrality()
 
     def find_descendants_bfs(self, root, u,depth=0):
@@ -72,34 +101,84 @@ class BFSA(method.Method):
                 self.descendants[root][u].update(self.descendants[root][c])
         self.descendants[root][u].add(u)
 
-    def get_likelihood_by_BFSA(self, nodes, p, q):
+    def get_likelihood_by_BFSA2(self, nodes):
+        permutations = itertools.permutations(nodes)
+        n = len(nodes)
+        for p in permutations:
+            permitted = True
+            for i in np.arange(1, n):
+                # None of nodes[0,..,i-1] are descendants of nodes[i]
+                result = [p[j] not in self.descendants[p[0]][p[i]] for j in np.arange(0, i)]
+                if False in result:
+                    permitted = False
+                    break
+
+                # for j in np.arange(1, i):
+                #     if p[j] in self.descendants[p[0]][p[i]]:
+                #         permitted = False
+                #         break
+            if permitted:
+                self.likelihoods[p[0]] += self.compute_likelihood(p)
+
+    def get_likelihood_by_BFSA(self,  nodes, p, q ):
         """extends Heap's permutation generating algorithm.
         Permitted permutation: each node has a certain depth at which it will be found.
-
         Args:
             nodes: a list of all nodes
             p: the starting index
             q: the end index
-
         Returns:
         """
         if p == q:
-            self.likelihoods[nodes[0]] += self.compute_likelihood(nodes)
-            print nodes
+            for i in np.arange(1, q+1):
+                # None of nodes[0,..,i-1] are descendants of nodes[i]
+                result = [nodes[j] not in self.descendants[nodes[0]][nodes[i]] for j in np.arange(0, i)]
+                if False in result:
+                    return
+
+                # for j in np.arange(1, i):
+                #     if nodes[j] in self.descendants[nodes[0]][nodes[i]]:
+                #         return
+            l = self.compute_likelihood(nodes)
+            self.likelihoods[nodes[0]].value += l
+            # self.permutation_likelihood[nodes[0]].append(l)
+            # self.values[nodes[0]].value = self.values[nodes[0]].value+l
+            # print nodes, l, self.likelihoods[nodes[0]]
+            return l
         else:
             for i in np.arange(p, q + 1):
-                if nodes[p] not in self.descendants[nodes[0]][nodes[i]]:
-                    # Nodes[p] is not a descendant of nodes[i], so we can exchange them.
-                    nodes[p], nodes[i] = nodes[i], nodes[p]
-                    self.get_likelihood_by_BFSA(nodes, p + 1, q)
-                    nodes[p], nodes[i] = nodes[i], nodes[p]
+                nodes[p], nodes[i] = nodes[i], nodes[p]
+                self.get_likelihood_by_BFSA(nodes, p + 1, q)
+                nodes[p], nodes[i] = nodes[i], nodes[p]
 
     def compute_likelihood(self, nodes):
-        """get P(jv = v) by Equation (11);
+        """get P(G|v) by Equation (11);
         Args:
-            nodes:
-
+            nodes: a permitted permutation
         Returns:
-
         """
-        return 0
+        likelihood = 1
+        neighbours = set()
+        neighbours.add(nodes[0])
+        w = {} # effective propagation probabilities: node->w
+        w[nodes[0]] = 1
+        visited = set()
+        for i in np.arange(0, len(nodes)):
+            u = nodes[i]
+            visited.add(u)
+            w_sum = sum([w[j] for j in neighbours])
+            likelihood *= w[u]/w_sum
+            neighbours.remove(u)
+            new = nx.neighbors(self.data.graph, u)
+
+            for h in new:
+                if h in visited:
+                    continue
+                # compute w for h
+                w_h2u = self.weights[self.node2index[u]][self.node2index[h]]
+                if w.has_key(h):
+                    w[h] = 1 - (1 - w[h]) * (1 - w_h2u)
+                else:
+                    w[h] = w_h2u
+                neighbours.add(h)
+        return likelihood
